a016f9d5bbe1707a313239071b0c65f61abf366c
zby <2844216106@qq.com>
2021-06-10 00:19:30 +0800

    1.4

---
+++ src/components/power/Roles.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <!--面包屑导航区域-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--卡片视图区域-->
+    <el-card>
+      <!--添加角色按钮区域-->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+
+      <!--角色列表区域-->
+      <el-table :data="roleList" border stripe>
+        <!--展开列-->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row :class="['bdbottom',i1 ===0 ?'bdtop':'']" v-for="(item1,i1) in scope.row.children" :key="item1.id">
+              <!--渲染一级权限-->
+              <el-col :span="5">
+                <el-tag>
+                  {{item1.authName}}
+                </el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!--渲染二级和三级权限-->
+              <el-col :span="19">
+              <!--渲染二级和三级权限-->
+                <el-row :class="[i2===0? '':'bdtop']" v-for="(item2,i2) in item1.children" :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag type="success">
+                      {{item2.authName}}
+                    </el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag type="warning" v-for="(item3,i3) in item2.children" :key="item3.id">
+                      {{item3.authName}}
+                    </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+            <pre>
+              {{scope.row}}
+            </pre>
+          </template>
+        </el-table-column>
+        <!--索引列-->
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+            <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+            <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      //权限列表
+      roleList:[]
+    }
+  },
+  created() {
+    //获取所有权限
+    this.getRolesList()
+  },
+  methods:{
+    //获取权限列表
+    async getRolesList(){
+      const {data:res}=await this.$http.get('roles')
+      if(res.meta.status!==200){
+        return this.$message.error('获取角色失败')
+      }
+      this.roleList=res.data
+      console.log(this.roleList)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-tag{
+  margin: 7px;
+}
+.bdtop{
+  border-top: 1px solid #eee;
+}
+.bdbottom{
+  border-bottom: 1px solid #eee;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,7 @@ import { Button, Form, FormItem, Input, Message,Container,
   Header,Aside,Main,Menu,Submenu
   ,MenuItem,Breadcrumb,BreadcrumbItem,Card,Row,Col,
   Table,TableColumn,Switch,Tooltip,Pagination,
-  Dialog,MessageBox} from 'element-ui'
+  Dialog,MessageBox,Tag} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -27,6 +27,7 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm=MessageBox.confirm
